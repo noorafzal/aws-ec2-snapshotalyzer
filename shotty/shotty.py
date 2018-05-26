bcf1c8880eebbd615a19bd1f4fd6380bf7fc6c65
Noor <Afzal.X.Noor.-ND@disney.com>
2018-05-26 01:18:16 -0400

    trying to use argv from sys package

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,8 +1,13 @@
 import boto3
+import sys
 
-if __name__ == '__main__':
-    session = boto3.Session(profile_name='shotty')
-    ec2 = session.resource('ec2')
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
 
+def list_intances():
     for i in ec2.instances.all():
         print(i)
+
+if __name__ == '__main__':
+    print(sys.argv)
+    list_intances()
