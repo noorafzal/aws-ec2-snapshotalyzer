5357bc99f9b643e129b5e37c4b338977e3e87a13
Noor <Afzal.X.Noor.-ND@disney.com>
2018-05-26 19:28:25 -0400

    Show all snapshots in list if --all is given

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -36,7 +36,8 @@ def instances():
 
 @snapshots.command('list')
 @click.option('--project', default=None, help="Only instances for project (tag Project:<name>)")
-def list_snapshots(project):
+@click.option('--all', 'list_all', default=False, is_flag=True, help="List all snapshots for each volume, not just the most recent ones")
+def list_snapshots(project, list_all):
     "List EBS snapshots"
     instances = filter_instances(project)
 
@@ -51,6 +52,8 @@ def list_snapshots(project):
                 s.progress,
                 s.start_time.strftime("%c")
                 )))
+
+                if s.state == 'completed' and not list_all: break
 
     return
 
