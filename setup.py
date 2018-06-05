df3efead9877738c67a788447a5b78b2e3ed049a
Noor <Afzal.X.Noor.-ND@disney.com>
2018-06-04 22:02:27 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     author="Afzal Noor",
     author_email="noor.afzal21@gmail.com",
-    description="SnapshotAlyzer 30000 is a tool to manage AWS EC2 snapshots",
+    description="SnapshotAlyzer is a tool to manage AWS EC2 snapshots",
     license="GPLv3",
     packages=['shotty'],
     url="https://github.com/noorafzal/aws-ec2-snapshotalyzer",
